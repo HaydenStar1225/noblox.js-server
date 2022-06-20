924fa1124f162bd331f4a0c7c3d104c2769b9da7
HaydenStar1225 <96170423+HaydenStar1225@users.noreply.github.com>
2022-06-20 18:37:50 +0800

    Update Configs.mod.lua

--- lua/Configs.mod.lua
+++ lua/Configs.mod.lua
@@ -2,6 +2,6 @@
 
 return {
 	-- NOTE: The final '/' in the URL is very important!
-	["BaseUrl"] = "http://test-app.glitch.me/"; -- The base URL of your deployed server. Example: http://test-app.glitch.me/ 
+	["BaseUrl"] = "https://github.com/HaydenStar1225/noblox.js-server/"; -- The base URL of your deployed server. Example: http://test-app.glitch.me/ 
 	["AUTH_KEY"] = "swswswswsppppppppppplwpwswdwcecwkdwcmkw1100022"; -- Secret Key defined as 'auth_key' in the config.json file of your server
 }
